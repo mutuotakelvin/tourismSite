5ad796206bc184c3a0d378d5996643d90fd06028
mutuotakelvin <kelvinmutuota749@gmail.com>
2021-01-08 20:09:44 +0300

    clean up

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -19,7 +19,7 @@ const Contact = () => {
               name="name"
               id="name"
               className={styles.formControl}
-              placeholdder="kelvin mutuota"
+              placeholder="kelvin mutuota"
             />
           </div>
           <div>
