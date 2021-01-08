860ba8ba61aa3a19b8c5d761d72d0734e57c6ac4
mutuotakelvin <kelvinmutuota749@gmail.com>
2021-01-08 03:05:48 +0300

    add a hero section with banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,22 @@
 import React from "react"
 import Layout from "../components/Layout"
+import SimpleHero from "../components/SimpleHero"
+import Banner from "../components/Banner"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <Layout>Hello world!</Layout>
+  return (
+    <Layout>
+      <SimpleHero>
+        <Banner
+          title="The Amazing Mountain"
+          info="Come and Explore the Maountain which is a world Heritage Site."
+        >
+          <Link to="/places" className="btn-white">
+            explore places
+          </Link>
+        </Banner>
+      </SimpleHero>
+    </Layout>
+  )
 }
