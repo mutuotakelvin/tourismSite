dd146aa519bd34a68712eaf786de64f6264d822f
mutuotakelvin <kelvinmutuota749@gmail.com>
2021-01-08 09:29:41 +0300

    update the about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,43 @@
 import React from "react"
 import Title from "./Title"
+import styles from "../css/about.module.css"
+import img from "../images/defaultBcg.jpg"
 
 const About = () => {
   return (
-    <div>
-      About components
+    <section className={styles.about}>
       <Title title="about" subtitle="maountain" />
-    </div>
+      <div className={styles.aboutCenter}>
+        <article className={styles.aboutImg}>
+          <div className={styles.imgContainer}>
+            <img src={img} alt="about company" />
+          </div>
+        </article>
+        <artical className={styles.aboutInfo}>
+          <h4>The Mount Kenya. </h4>
+          <p>
+            Climbing to 5,199 meters, Mount Kenya is the second tallest mountain
+            in Africa. The scenery surrounding this designated World Heritage
+            Site is breath-taking. It is pristine wilderness with lakes, tarns,
+            glaciers, dense forest, mineral springs and a selection of rare and
+            endangered species of animals, high altitude adapted plains game and
+            unique montane and alpine vegetation.
+          </p>
+          <p>
+            Visitors can enjoy mountain climbing, camping and caving with the
+            mountain’s rugged glacier-clad peaks providing the perfect backdrop.
+          </p>
+          <a
+            href="https://en.wikipedia.org/wiki/Mount_Kenya"
+            className="btn-primary"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            read more
+          </a>
+        </artical>
+      </div>
+    </section>
   )
 }
 export default About
