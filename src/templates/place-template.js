2c745da30e94e46c75b3ab267f6f8ee097a1a95f
mutuotakelvin <kelvinmutuota749@gmail.com>
2021-01-14 01:03:54 +0300

    place variable

--- src/templates/place-template.js
+++ src/templates/place-template.js
@@ -1,5 +1,11 @@
 import React from "react"
 import { graphql } from "gatsby"
+import Layout from "../components/Layout"
+import StyledHero from "../components/StyledHero"
+import styles from "./../css/template.module.css"
+import Img from "gatsby-image"
+import { FaMoneyBillWave, FaClock, FaTypo3 } from "react-icons/fa"
+import AniLink from "gatsby-plugin-transition-link/AniLink"
 
 const Template = ({ data }) => {
   const {
@@ -12,12 +18,55 @@ const Template = ({ data }) => {
   } = data.place
 
   const [mainImage, ...placeImages] = images
-  return <h1>{name}</h1>
+  return (
+    <Layout>
+      <StyledHero img={mainImage.fluid} />
+      <section className={styles.template}>
+        <div className={styles.center}>
+          <div className={styles.images}>
+            {placeImages &&
+              placeImages.map((item, index) => {
+                return (
+                  <Img
+                    key={index}
+                    fluid={item.fluid}
+                    alt="single"
+                    className={styles.image}
+                  />
+                )
+              })}
+          </div>
+          <h2>{name}</h2>
+          <div className={styles.info}>
+            <p>
+              <FaMoneyBillWave className={styles.icon} />
+              Entry Fees - {entryFees}
+            </p>
+            <p>
+              <FaClock className={styles.icon} /> Time Required - {timeRequired}{" "}
+              hours
+            </p>
+            {timings ? (
+              <p>
+                <FaTypo3 className={styles.icon} />
+                Timings - {timings}
+              </p>
+            ) : (
+              <p className={styles.desc}>{description}</p>
+            )}
+            <AniLink fade to="/places" className="btn-primary">
+              back to places
+            </AniLink>
+          </div>
+        </div>
+      </section>
+    </Layout>
+  )
 }
 
-export const data = graphql`
+export const query = graphql`
   query($slug: String!) {
-    contentfulAmazingMountainData(slug: { eq: $slug }) {
+    place: contentfulAmazingMountainData(slug: { eq: $slug }) {
       name
       slug
       timeRequired
